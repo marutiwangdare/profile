22f8882bdc65914d6d73c724905be9f950c2fd5e
marutiwangdare <marutiedu999@gmail.com>
2023-01-25 16:55:35 +0530

    regular update commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,31 @@
 var settings_menu = document.querySelector(".settings-menu");
 
-// For the Create Post Pop-up for Desktop
-var clickInput = document.querySelector(".input-btn");
-var inputFocus = document.querySelector(".new-post-input textarea");
-var flex = document.querySelector(".flex");
-var closes = document.querySelector(".close-function");
-
-
-
-
 function settings_menu_toggle(){
     settings_menu.classList.toggle("settings-menu-height")
 }
-/*
-clickInput.onclick = ()=>{
-    flex.style.display = "block";
-    inputFocus.focus();
+
+var openProfile = document.querySelector(".user-profile-page-action");
+
+function openProfileAction(){
+    openProfile.classList.toggle("user-profile-page-action-click");
 }
 
-closes.onclick = ()=>{
-    flex.style.display = "none";
+function closeProfile(){
+    openProfile.classList.remove("user-profile-page-action-click");
 }
-*/
+
+
+// =================== For Desktop Profile Edit
+var editPop = document.querySelector(".user-profile-edit-pop-up-bg");
+
+function editPopUp(){
+    editPop.style.display = "block";
+} 
+
+function closeEditPopUp(){
+    editPop.style.display = "none";
+}
+
 
 // Dark Mode and Light Mode Swtich 
 var darkModeSwitch = document.querySelector("#dark-btn");
@@ -53,4 +57,48 @@ darkModeSwitch.onclick = ()=>{
     }
 
 }
+// End of Dark Mode and Light Mode Swtich
+
+// Mobile Menu 
+
+var mobileMenu = document.querySelector(".mobile-menu");
+var mobileMenuBtn = document.querySelector(".mobile-menu-open");
+var mobileMenuCloseBtn = document.querySelector(".mobile-menu-close-btn");
+
+mobileMenuBtn.onclick = ()=>{ 
+    mobileMenu.classList.toggle("mobile-menu-height");
+}
+
+mobileMenuCloseBtn.onclick = ()=>{
+    mobileMenu.classList.remove("mobile-menu-height");
+}
+
+// Dark Mode and Light Mode Swtich 
+var darkModeSwitch2 = document.querySelector("#dark-btn2");
+
+// Dark Mode for For Mobile
+if(localStorage.getItem("theme") == "light"){
+    darkModeSwitch2.classList.remove("clicked");
+    document.body.classList.remove("dark-mode-color");
+}
+else if(localStorage.getItem("theme") == "dark"){
+    darkModeSwitch2.classList.add("clicked");
+    document.body.classList.add("dark-mode-color");
+}
+else{
+    localStorage.setItem("theme", "light"); 
+}
+
+darkModeSwitch2.onclick = ()=>{
+    darkModeSwitch2.classList.toggle("clicked");
+    document.body.classList.toggle("dark-mode-color");
+
+    if(localStorage.getItem("theme") == "light"){
+        localStorage.setItem("theme", "dark");
+    }
+    else if(localStorage.getItem("theme") == "dark"){
+        localStorage.setItem("theme", "light");
+    }
+}
+
 // End of Dark Mode and Light Mode Swtich
